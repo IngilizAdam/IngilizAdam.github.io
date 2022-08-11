1ae1d635bbc6f5cc410df9904dcd61fadf5715a7
Hasan Tuna <hasantuna0070@gmail.com>
2022-08-11 06:22:28 +0300

    First Advanced API Test!

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const app = express();
-//const fs = require("fs");
 const PORT = 80;
 
 app.use(express.json());
 
 var data = ["red:12", "green:34", "blue:56"];
+var salonIsik = "1023";
 
 app.get("/desk/:led", (request, response) => {
-    /*const contents = fs.readFileSync("test.txt", "utf-8").split("\n");
-    for(let i = 0; i < contents.length; i++) {
-        const content = contents[i];
-        if(content.split(":")[0] == request.params.led) {
-            response.status(200).send(content.split(":")[1]);
-            console.log(content.split(":")[1]);
-            break;
-        }
-    }*/
     for(let i = 0; i < data.length; i++) {
         const content = data[i];
         if(content.split(":")[0] == request.params.led) {
@@ -30,32 +21,33 @@ app.get("/desk/:led", (request, response) => {
 app.get("/desk/:led/:id", (request, response) => {
     const led = request.params.led;
     const id = request.params.id;
-    console.log(request.params);
-    
-    /*var text = "";
-    const contents = fs.readFileSync("test.txt", "utf-8").trim().split("\n");
-    for(let i = 0; i < contents.length; i++) {
-        var content = contents[i];
-        if(content.split(":")[0] == request.params.led) {
-            content = led + ":" + id;
-        }
-        text += content + "\n";
-    }
-
-    fs.writeFile('test.txt', text, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });*/
 
     for(let i = 0; i < data.length; i++) {
         var content = data[i];
         if(content.split(":")[0] == request.params.led) {
             data[i] = led + ":" + id;
+            console.log(id);
+            response.send(id);
             break;
         }
     }
+});
 
-    response.send(id);
+app.get("/salon/isik", (request, response) => {
+    response.status(200).send(salonIsik);
+});
+
+app.get("/salon/isik/:id", (request, response) => {
+    const id = request.params.id;
+
+    if(id == "ac" || id == "1" || id == "1023") {
+        salonIsik = "1023";
+    }
+    else if(id == "kapat" || id == "0") {
+        salonIsik = "0";
+    }
+
+    response.status(200).send(salonIsik);
 });
 
 app.listen(
