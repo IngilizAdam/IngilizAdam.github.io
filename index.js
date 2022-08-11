d8f073e43fbf8e9036a5cc7cc85e2bf97f417fd2
Hasan Tuna <hasantuna0070@gmail.com>
2022-08-11 05:57:59 +0300

    First API Test!

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 const PORT = 80;
 
 app.use(express.json());
 
-app.get("/tshirt", (request, response) => {
-    response.status(200).send({
-        tshirt: "black",
-        size: "large"
-    });
+app.get("/desk/:led", (request, response) => {
+    const contents = fs.readFileSync("test.txt", "utf-8").split("\n");
+    for(let i = 0; i < contents.length; i++) {
+        const content = contents[i];
+        if(content.split(":")[0] == request.params.led) {
+            response.status(200).send(content.split(":")[1]);
+            console.log(content.split(":")[1]);
+            break;
+        }
+    }
 });
 
-app.post("/tshirt/:id", (request, response) => {
-    const {id} = request.params;
-    const {logo} = request.body;
+app.get("/desk/:led/:id", (request, response) => {
+    const led = request.params.led;
+    const id = request.params.id;
+    console.log(request.params);
+    
+    var text = "";
+    const contents = fs.readFileSync("test.txt", "utf-8").trim().split("\n");
+    for(let i = 0; i < contents.length; i++) {
+        var content = contents[i];
+        if(content.split(":")[0] == request.params.led) {
+            content = led + ":" + id;
+        }
+        text += content + "\n";
+    }
 
-    localStorage.setItem('id', JSON.stringify(id));
+    fs.writeFile('test.txt', text, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 
-    response.send({tshirt: "a with your " + JSON.parse(localStorage.getItem('id')) + " and ID of " + id});
+    response.send(id);
 });
 
 app.listen(
